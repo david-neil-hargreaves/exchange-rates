ea0c6287714324f2fed25d1f44c9e21267753735
David Hargreaves <david_hargreaves@talk21.com>
2020-06-30 09:43:42 +0100

    app component unit tests pass.

--- exchange-rates-client/src/app/app.component.spec.ts
+++ exchange-rates-client/src/app/app.component.spec.ts
@@ -1,10 +1,28 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+
+@Component({selector: 'app-banner', template: ''})
+class BannerStubComponent {}
+
+@Component({selector: 'router-outlet', template: ''})
+class RouterOutletStubComponent { }
+
+@Component({selector: 'app-welcome', template: ''})
+class WelcomeStubComponent {}
+
+@Component({selector: 'app-welcome', template: ''})
+class RouterLinkDirectiveStub {}
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+		RouterLinkDirectiveStub,
+        BannerStubComponent,
+        RouterOutletStubComponent,
+        WelcomeStubComponent
       ],
     }).compileComponents();
   }));
@@ -13,15 +31,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Exchange Rates'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Exchange Rates');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in an h2 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h2').textContent).toContain('Exchange Rates');
   }));
 });
