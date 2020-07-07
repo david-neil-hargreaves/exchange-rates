bef2c9c5ca0af725bd86545049e0f157ddad2acf
David Hargreaves <david_hargreaves@talk21.com>
2020-07-07 13:54:42 +0100

    End-to-end tests - w.i.p.

--- exchange-rates-client/e2e/app.e2e-spec.ts
+++ exchange-rates-client/e2e/app.e2e-spec.ts
@@ -1,14 +1,24 @@
 import { AppPage } from './app.po';
 
 describe('exchange-rates-client App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    it('should display home screen', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('Exchange Rates');
+    });
+
+    it('should display menu screen', () => {
+        page.navigateToMenu();
+    });
+
+    it('should select subject currency', () => {
+        page.selectSubjectCurrency();
+    });
+
+
 });
--- exchange-rates-client/e2e/app.po.ts
+++ exchange-rates-client/e2e/app.po.ts
@@ -1,11 +1,20 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+
   navigateTo() {
     return browser.get('/');
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root h2')).getText();
+  }
+
+  navigateToMenu(){
+      return element(by.linkText('Menu')).click();
+  }
+
+  selectSubjectCurrency(){
+    element(by.id('subjectCurrency')).click();
   }
 }
